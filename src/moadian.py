b6ea7d68f8683369725035edd39ce4fd841d01da
mojtaba <mojtabakarimi1997@gmail.com>
2023-06-20 21:22:48 +0330

    feat: define get_economic_code_information method of Moadian class

--- src/moadian.py
+++ src/moadian.py
@@ -9,9 +9,9 @@ PRIORITIES = ["normal", "fast"]
 class Moadian():
     base_url = "https://tp.tax.gov.ir/req/api/{mode}/sync"
 
-    def __init__(self, username, private_key, mode="self-tsp") -> None:
+    def __init__(self, fiscal_id, private_key, mode="self-tsp") -> None:
         self.private_key = private_key
-        self.username = username
+        self.fiscal_id = fiscal_id
         self.data = {"time": 1, "signature": ""}
         self.base_url = self.base_url.format(mode=mode)
 
@@ -20,6 +20,14 @@ class Moadian():
         headers = init_headers()
         data = {"time": 1, "signature": ""}
         data["packet"] = Packet("GET_SERVER_INFORMATION",
-                                self.username).to_dict()
+                                self.fiscal_id).to_dict()
         r = send_req(url, headers, data)
+        return json.loads(r.text)
+
+    def get_economic_code_information(self, economic_code):
+        url = self.base_url + "/GET_ECONOMIC_CODE_INFORMATION"
+        headers = init_headers()
+        self.data["packet"] = Packet("GET_ECONOMIC_CODE_INFORMATION", self.fiscal_id, {
+            "economicCode": economic_code},).to_dict()
+        r = send_req(url, headers, self.data)
         return json.loads(r.text)
