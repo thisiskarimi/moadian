5e91848f12ae154a82e71915b9c3975a38e90788
mojtaba <mojtabakarimi1997@gmail.com>
2023-06-20 21:00:05 +0330

    feat: verhoeff algorithm

---
+++ src/verhoeff.py
@@ -0,0 +1,44 @@
+
+MULTIPLICATION_TABLE = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 0, 6, 7, 8, 9, 5],
+    [2, 3, 4, 0, 1, 7, 8, 9, 5, 6],
+    [3, 4, 0, 1, 2, 8, 9, 5, 6, 7],
+    [4, 0, 1, 2, 3, 9, 5, 6, 7, 8],
+    [5, 9, 8, 7, 6, 0, 4, 3, 2, 1],
+    [6, 5, 9, 8, 7, 1, 0, 4, 3, 2],
+    [7, 6, 5, 9, 8, 2, 1, 0, 4, 3],
+    [8, 7, 6, 5, 9, 3, 2, 1, 0, 4],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+]
+
+PERMUTATION_TABLE = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 5, 7, 6, 2, 8, 3, 0, 9, 4],
+    [5, 8, 0, 3, 7, 9, 6, 1, 4, 2],
+    [8, 9, 1, 6, 0, 4, 3, 5, 2, 7],
+    [9, 4, 5, 3, 1, 2, 6, 8, 7, 0],
+    [4, 2, 8, 6, 5, 7, 3, 9, 0, 1],
+    [2, 7, 9, 3, 8, 0, 6, 4, 1, 5],
+    [7, 0, 4, 6, 9, 1, 3, 2, 5, 8],
+]
+
+INVERSE_TABLE = [0, 4, 3, 2, 1, 5, 6, 7, 8, 9]
+
+
+def checkSum(number):
+    c = 0
+    len_ = len(number)
+    for i in range(len_):
+        c = MULTIPLICATION_TABLE[c][PERMUTATION_TABLE[(
+            (i + 1) % 8)][int(number[len_ - i - 1]) - 0]]
+    return INVERSE_TABLE[c]
+
+
+def validate(number):
+    c = 0
+    len = len(number)
+    for i in range(len):
+        c = MULTIPLICATION_TABLE[c][PERMUTATION_TABLE[(
+            i % 8)][int(number[len - i - 1]) - 0]]
+    return c == 0
