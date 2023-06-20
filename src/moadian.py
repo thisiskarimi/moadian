58383542edc75b59d116c8713856956c12f770ad
mojtaba <mojtabakarimi1997@gmail.com>
2023-06-20 21:18:32 +0330

    feat: define get_server_information method of Moadian class

--- src/moadian.py
+++ src/moadian.py
@@ -1,3 +1,6 @@
+import json
+from .packet import Packet
+from .http import init_headers, send_req
 
 
 PRIORITIES = ["normal", "fast"]
@@ -11,3 +14,12 @@ class Moadian():
         self.username = username
         self.data = {"time": 1, "signature": ""}
         self.base_url = self.base_url.format(mode=mode)
+
+    def get_server_information(self):
+        url = self.base_url + "/GET_SERVER_INFORMATION"
+        headers = init_headers()
+        data = {"time": 1, "signature": ""}
+        data["packet"] = Packet("GET_SERVER_INFORMATION",
+                                self.username).to_dict()
+        r = send_req(url, headers, data)
+        return json.loads(r.text)
