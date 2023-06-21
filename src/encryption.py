07abbb7e2d8aeca7e715c251ecc13349307f64e5
mojtaba <mojtabakarimi1997@gmail.com>
2023-06-22 00:10:38 +0330

    feat: modify sign function

--- src/encryption.py
+++ src/encryption.py
@@ -1,8 +1,11 @@
-import rsa
 import base64
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
 
 
 def sign(data, private_key):
-    pkey = rsa.PrivateKey.load_pkcs1(private_key)
-    sign = rsa.sign(data.encode(), pkey, "SHA-256")
-    return base64.b64encode(sign).decode()
+    key = RSA.import_key(private_key)
+    h = SHA256.new(data.encode())
+    signature = pkcs1_15.new(key).sign(h)
+    return base64.b64encode(signature).decode()
